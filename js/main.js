6287ed9dae5c7f4d70301f2483aa40d5343de817
AsimoLoveGym <xiazhuangz@gmail.com>
2016-10-02 16:29:32 -0500

    Feat: draftly finish the core calculating part, but need a mroe feneral function for all cases
    Todo: need to handle negative numbers, limit calculating, limit input, error input handling

--- js/main.js
+++ js/main.js
@@ -46,6 +46,7 @@ function calculating(equationString) {
   var operatorsIndex = -1;
   var subStringOIndex = -1;
   var slicedString = "";
+  var calculatingResult = 0;
   // for Test
   equationString = "1+2x3-6÷2+5";
   numbersArray = equationString.split(operatorPattern);
@@ -65,10 +66,37 @@ function calculating(equationString) {
     }
     // console.log(subStringOIndex);
     operatorsIndex += subStringOIndex+1;
+
     // console.log(operatorsIndex);
   }
   console.log(operatorsArray);
+  // console.log("1+3: ",Number("1+3"));
+  for(var i = 0; i < operatorsArray.length; i ++) {
+    var num1 = Number(numbersArray[0]);
+    numbersArray.shift();
+    var num2 = Number(numbersArray[0]);
+    numbersArray.shift();
+    console.log("Before Calculating",numbersArray);
 
+    /\+|\-|x|÷/
+    switch (operatorsArray[i]){
+      case "+":
+        calculatingResult = num1 + num2;
+        break;
+      case "-":
+        calculatingResult = num1 - num2;
+        break;
+      case "x":
+        calculatingResult = num1 * num2;
+        break;
+      case "÷":
+        calculatingResult = num1 / num2;
+        break;
+    }
+    console.log(calculatingResult);
+    numbersArray.unshift(calculatingResult);
+    console.log("After Calculating",numbersArray);
+  }
 };
 
 function display(secondDisplay, mainDisplay){

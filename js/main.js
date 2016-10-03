59e6a8a7bc5c4906125c2870d5292ddd09267d2c
AsimoLoveGym <xiazhuangz@gmail.com>
2016-10-03 01:53:18 -0500

    Doc: add comments on the main.js code, for explaing how the calculating process works
    Todo: Display need more work, eg. display limit, JS float number issue

--- js/main.js
+++ js/main.js
@@ -1,52 +1,85 @@
 var input = "";
 var currentInput = "";
-var lastOperator = "";
+// var lastOperator = "";
 var operatorPattern = /\+|\-|x|÷/;
-var operatorPatternGlobal = /\+|\-|x|÷/g;
+// Initia input in a calculator is always a number
 var expectNumber = true;
 var expectOperator = false;
 var expectCalculating = false;
+// afterCalculating flag would be used in calculating result utilization
+// if you'd like to further work on a result, input operator
+// if you'd like to start a new calculation, input a number; previous result will be removed.
 var afterCalculating = false;
 
+// ********************** Below for dealing with Input ************************
 $(document).ready(function(){
   $(".btn").click(function(event){
-    // console.log(event);
     var singleInput = "";
-    // console.log("click function works!!");
     singleInput = event.currentTarget.innerHTML;
     var numberPattern = /\d|\./;
 
     if (numberPattern.test(singleInput) && expectNumber) {
-      // it's a number or '.'
+      // The input is a number or '.'
       if (singleInput === "0" && currentInput.length === 0) {
+        // In the case the first input is 0, it's a number without meaning
         // Do nothing. Invalid input, no need to update input and currentInput
       } else if (singleInput === "." &&currentInput.indexOf(".") > -1) {
+        // If current number is already the one with point, meaningless to have two point in a number
+        // So, ignore this point
         // Do nothing. Invalid input, no need to update input and currentInput
       } else if (afterCalculating) {
+        // remove the previous calculating result
+        // start a new calculation
         input = singleInput;
         currentInput = singleInput;
         afterCalculating = false;
         expectOperator = true;
         expectCalculating = true;
+        if (currentInput === ".") {
+          // a point "." is not a complete number for calculating
+          expectCalculating = false;
+        }
+
       } else {
+        // valid input, update the First Line Display & Second Line Display
+        // Second Line Display updating
         input += singleInput;
+        // First Line Display updating
         currentInput += singleInput;
+        // already have a valid number in expression, ready for operator
         expectOperator = true;
+        // ready for calculating
         expectCalculating = true;
+        if (currentInput === ".") {
+          // a point "." is not a complete number for calculating
+          expectCalculating = false;
+        }
       }
-    } else if (operatorPattern.test(singleInput) && expectOperator && input !== ".") {
-      // it's a math operator
+    } else if (operatorPattern.test(singleInput) && expectOperator && currentInput !== ".") {
+      // The input is a math operator
+      // The expectOperator should be turned on
+      // The number waiting for operator should not be a single "point"
       if (afterCalculating) {
-        lastOperator = singleInput;
+
+        // lastOperator = singleInput;
+        // The currentInput value is previous calculation result
+        // give the input with the result value for further falculating
         input = currentInput;
+        // update the input value with the operator just input
         input += singleInput;
+        // reset the currentInput value
         currentInput = "";
+        // reset the afterCalculating value
         afterCalculating = false;
+        // once input with just one operator, we need a number to be followed
         expectOperator = false;
       } else {
-        lastOperator = singleInput;
+        // lastOperator = singleInput;
+        // update the input value with the operator just input
         input += singleInput;
+        // reset the currentInput value
         currentInput = "";
+        // once input with just one operator, we need a number to be followed
         expectOperator = false;
       }
 
@@ -55,7 +88,7 @@ $(document).ready(function(){
       // Reset all the control flags and initial values
       input = "";
       currentInput = "";
-      lastOperator = "";
+      // lastOperator = "";
       expectNumber = true;
       expectOperator = false;
       expectCalculating = false;
@@ -67,33 +100,43 @@ $(document).ready(function(){
         while (numberPattern.test(input.charAt(input.length-1))){
           input = input.slice(0,input.length-1);
         }
+        // since last input number has been removed, we expect a new number
         expectOperator = false;
       } else if (operatorPattern.test(input.charAt(input.length-1))) {
+        // if last character of input is operator, remove it
         input = input.slice(0,input.length-1);
         expectOperator = true;
       }
-      // var lastIndexOfOperator = input.lastIndexOf(lastOperator);
-      // input = input.slice(0, lastIndexOfOperator+1);
+      // Reset the currentInput
       currentInput = "";
-
     } else if (singleInput === "=" && expectCalculating) {
-      // console.log("it's calculating mode");
+      // it's calculating mode
+      // expectCalculating flag is on, this is always set to be on with a valid number input
+      // But in a special case I, the calculating is not necessary
+      // if it's just one number without operator, no need to calculating;
+
+
       if(operatorPattern.test(input)) {
+        // calculating is valid when
+        // First, operator existed in input
         console.log("Calculating.....");
         currentInput = calculating(input);
+        // after calculating, reset all the important flags
         expectNumber = true;
         expectOperator = true;
         expectCalculating = false;
         afterCalculating = true;
       }
     }
-    // console.log(input);
-    // console.log(currentInput);
-
+    // after dealing with the singleInput
+    // currentInput value and input value has been updated
+    // display them in the display unit
     display(input, currentInput);
   });
 });
+// ********************** Above for dealing with Input ************************
 
+// ********************** Below for calculating ************************
 function calculating(equationString) {
   var numbersArray = [];
   var operatorsArray = [];
@@ -101,38 +144,55 @@ function calculating(equationString) {
   var subStringOIndex = -1;
   var slicedString = "";
   var calculatingResult = 0;
-  // for Test
+  // for fast Test
   // equationString = "78+24x12-54+343+34";
+  // split the equationString, and grap the numbers into an array
   numbersArray = equationString.split(operatorPattern);
-  console.log(numbersArray);
+  // for testing
+  // console.log(numbersArray);
 
+  // String.search() is always started from the begining
+  // and always stoped at the first matcher
   operatorsIndex = equationString.search(operatorPattern);
+  // index in substring for operator
   subStringOIndex = operatorsIndex;
+  // extract the operator into an arrays like the number arrays
   operatorsArray.push(equationString.charAt(operatorsIndex));
-  // var num = 0;
+  // keep extract operators if substring has operators
   while (subStringOIndex>-1) {
     slicedString = equationString.slice(operatorsIndex+1);
+    // for testing
     // console.log(equationString);
     // console.log(slicedString);
     subStringOIndex = slicedString.search(operatorPattern);
     if (subStringOIndex !== -1) {
+      // operator found in the subString
       operatorsArray.push(slicedString.charAt(subStringOIndex));
     }
+    // for testing
     // console.log(subStringOIndex);
+    // update operatorsIndex value
     operatorsIndex += subStringOIndex+1;
-
+    // for testing
     // console.log(operatorsIndex);
   }
-  console.log(operatorsArray);
+  // for testing
+  // console.log(operatorsArray);
   // console.log("1+3: ",Number("1+3"));
+numbersArray
+  // Calculating with extracted operatorsArray and numbersArray
+  // there are how many operators in operatorsArray, there should be the same
+  // number of calculating exist.
   for(var i = 0; i < operatorsArray.length; i ++) {
+    // extract the number from numbersArray, ready for calculating
     var num1 = Number(numbersArray[0]);
+    // removed the number has been utilized
     numbersArray.shift();
     var num2 = Number(numbersArray[0]);
     numbersArray.shift();
-    console.log("Before Calculating",numbersArray);
+    // console.log("Before Calculating",numbersArray);
 
-    // /\+|\-|x|÷/
+    // Different calculating with different operators
     switch (operatorsArray[i]){
       case "+":
         calculatingResult = num1 + num2;
@@ -147,13 +207,14 @@ function calculating(equationString) {
         calculatingResult = num1 / num2;
         break;
     }
-    console.log(calculatingResult);
-    numbersArray.unshift(calculatingResult);
-    console.log("After Calculating",numbersArray);
+    // console.log(calculatingResult);
+    // console.log("After Calculating",numbersArray);
     return calculatingResult;
   }
 };
+// ********************** Above for calculating ************************
 
+// ********************** Below for displaying ************************
 function display(secondDisplay, mainDisplay){
   if(secondDisplay === "") {
     secondDisplay = "0";
@@ -164,3 +225,4 @@ function display(secondDisplay, mainDisplay){
   $("#main-display").html(mainDisplay);
   $("#second-display").html(secondDisplay);
 }
+// ********************** Above for displaying ************************
